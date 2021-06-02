95f9ff3da0f8b45b90970771aa6f7da1a0ea136f
Adam Gwilliam <adamgwilliam@gmail.com>
2021-06-02 18:48:15 -0500

    fix page title, add alt text for resume image

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
 import './App.css';
 import { Route, Redirect, BrowserRouter } from 'react-router-dom'
 import Resume from './pages/Resume'
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Route exact path="/">
-          <Redirect to="/resume" />
-        </Route>
-        <Route exact path="/resume">
-          <Resume />
-        </Route>
-      </BrowserRouter>
-    </div>
-  );
+class App extends React.Component {
+  componentDidMount() {
+    document.title = "Adam Gwilliam";
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <Route exact path="/">
+            <Redirect to="/resume" />
+          </Route>
+          <Route exact path="/resume">
+            <Resume />
+          </Route>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -8,7 +8,7 @@ export default class Resume extends React.Component {
         return (
             <div className="container">
                 <Link to="/assets/AdamGwilliamResume.pdf" target="_blank" download>
-                    <img className="resume" title="click to download" src={resume} />
+                    <img className="resume" title="click to download" src={resume} alt="resume for Adam Gwilliam"/>
                 </Link>
             </div>
         );
